8cd62b25499455f2c35bf79b30b366654a4c507e
tekehiko69 <tekehiko69@gmail.com>
2022-01-28 17:28:32 +0300

    new transition between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,12 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 from locators import MainPageLocator
+from login_page import LoginPage
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocator.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
