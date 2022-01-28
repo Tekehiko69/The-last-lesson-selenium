b7549fd922785a9d72b42008c1dfbc80bff939ae
tekehiko69 <tekehiko69@gmail.com>
2022-01-28 17:31:41 +0300

    create handler alert

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,13 +1,14 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 from locators import MainPageLocator
-from login_page import LoginPage
+
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocator.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocator.LOGIN_LINK), "Login link is not presented"
